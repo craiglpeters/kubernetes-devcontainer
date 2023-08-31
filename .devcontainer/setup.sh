5ee9ffd960506b18e9ec5404f2528d60e58f5383
Craig Peters <craiglpeters@github.com>
2023-08-31 19:02:20 +0000

    remove the container image prebuild

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -16,12 +16,16 @@
 
 set -eux
 
+echo "Cloning kubernetes/kubectl" && mkdir -p /go/src/sigs.k8s.io && cd /go/src/sigs.k8s.io && git clone --depth=1 https://github.com/kubernetes/kubectl
+echo "Cloning golang/term" && cd /go/src && git clone --depth=1 https://github.com/golang/term.git
+
 # Install pyyaml as required by verify.publishing-bot
 python3 -m pip install --user --upgrade --no-cache-dir pyyaml
 
 # Copies over welcome message
 cp .devcontainer/welcome-message.txt /usr/local/etc/vscode-dev-containers/first-run-notice.txt
 
+# Finish git setup as per https://www.kubernetes.dev/docs/guide/github-workflow/
 git remote add upstream https://github.com/kubernetes/kubernetes.git
 # Never push to upstream master
 git remote set-url --push upstream no_push
